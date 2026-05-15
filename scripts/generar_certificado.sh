f9c253c439f940bc5f0f16bcc596fd7e17b23b97
Marina <mtamaralg@ip-172-31-37-164.ec2.internal>
2026-05-15 10:19:22 +0000

    terminada

---
+++ scripts/generar_certificado.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -x
+
+
+
+# Creamos el certificado autofirmado
+sudo openssl req \
+  -x509 \
+  -nodes \
+  -days 365 \
+  -newkey rsa:2048 \
+  -keyout /etc/ssl/private/apache-selfsigned.key \
+  -out /etc/ssl/certs/apache-selfsigned.crt \
+  -subj "/C=$OPENSSL_COUNTRY/ST=$OPENSSL_PROVINCE/L=$OPENSSL_LOCALITY/O=$OPENSSL_ORGANIZATION/OU=$OPENSSL_ORGUNIT/CN=$OPENSSL_COMMON_NAME/emailAddress=$OPENSSL_EMAIL"
